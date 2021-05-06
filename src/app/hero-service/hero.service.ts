8212d7dd8ce7d13631d24633fb83ede6f2411b91
Klaas Decock <klaasdc@icloud.com>
2021-05-06 10:41:58 +0200

    5) Routing: routing to hero detail page

--- src/app/hero-service/hero.service.ts
+++ src/app/hero-service/hero.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Hero} from "../model/hero";
 import {HEROES} from "../model/mock-heroes";
 import {Observable, of} from "rxjs";
@@ -9,11 +9,16 @@ import {MessageService} from "../message-service/message.service";
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService) {
+  }
 
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
+  }
+
+  findHeroById(id: number): Observable<Hero> {
+    return of(HEROES.find(hero => hero.id === id) as Hero);
   }
 
 }
